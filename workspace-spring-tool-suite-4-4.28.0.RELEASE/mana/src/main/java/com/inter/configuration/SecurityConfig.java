233cec2c4afcd5ea1645bca44c923967f2ac226b
Idaliz Aviles <idalizk.v@gmail.com>
2025-04-30 23:25:21 -0700

    A couple of files were missing

---
+++ workspace-spring-tool-suite-4-4.28.0.RELEASE/mana/src/main/java/com/inter/configuration/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.inter.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> {
+			auth.requestMatchers("/").permitAll();
+			auth.requestMatchers("/login").permitAll();
+			auth.requestMatchers("/register").permitAll();
+			auth.anyRequest().authenticated();
+		}).formLogin(form -> {
+			form.loginPage("/login").permitAll();
+			form.defaultSuccessUrl("/user").permitAll();
+			form.failureUrl("/login?error=true").permitAll();
+		}).logout(logout -> {
+			logout.logoutUrl("/logout").permitAll();
+			logout.logoutSuccessUrl("/").permitAll();
+		});
+
+		return http.build();
+	}
+}
