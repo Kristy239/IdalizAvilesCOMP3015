c84c89a34e46fd8e2ae306229dfca914b141623b
Idaliz Aviles <idalizk.v@gmail.com>
2025-05-02 13:17:40 -0700

    Made the account page operational

--- workspace-spring-tool-suite-4-4.28.0.RELEASE/mana/src/main/java/com/inter/controller/AccountController.java
+++ workspace-spring-tool-suite-4-4.28.0.RELEASE/mana/src/main/java/com/inter/controller/AccountController.java
@@ -1,6 +1,9 @@
 package com.inter.controller;
 
+import java.util.Base64;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,12 +11,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.inter.model.Advert;
 import com.inter.model.User;
 import com.inter.repository.AdvertCategoryRepository;
+import com.inter.repository.AdvertRepository;
 
 @Controller
 @RequestMapping("account")
 public class AccountController {
+
+	@Autowired
+	AdvertRepository advertRepository;
 
 	@Autowired
 	AdvertCategoryRepository advertCategoryRepository;
@@ -22,8 +30,57 @@ public class AccountController {
 	public String account(Map<String, Object> model) {
 		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		model.put("user", user);
+		List<AdvertCard> adverts = advertRepository.findByOwner(user).stream().map(advert -> new AdvertCard(advert))
+				.collect(Collectors.toList());
+		model.put("adverts", adverts);
 		model.put("advertCategories", advertCategoryRepository.findAll());
 
 		return "account";
 	}
+}
+
+class AdvertCard {
+	private Long id;
+	private String title;
+	private String image;
+
+	public AdvertCard() {
+		super();
+	}
+
+	public AdvertCard(Advert advert) {
+		super();
+		this.setId(advert.getID());
+		this.setTitle(advert.getTitle());
+		this.setImage(advert.getImage());
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public void setImage(byte[] image) {
+		this.image = Base64.getEncoder().encodeToString(image);
+	}
+
 }
--- workspace-spring-tool-suite-4-4.28.0.RELEASE/mana/src/main/java/com/inter/repository/AdvertRepository.java
+++ workspace-spring-tool-suite-4-4.28.0.RELEASE/mana/src/main/java/com/inter/repository/AdvertRepository.java
@@ -3,11 +3,13 @@ package com.inter.repository;
 import com.inter.model.Advert;
 import com.inter.model.User;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import jakarta.transaction.Transactional;
 
 @Transactional
 public interface AdvertRepository extends CrudRepository<Advert, Long> {
 	
-	public <List>Advert findByOwner(User owner);
+	public List<Advert> findByOwner(User owner);
 }
