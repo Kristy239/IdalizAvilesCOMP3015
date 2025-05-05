305154138fe7c00295edc688ffe0e6b2e99555b0
Kristy239 <idalizk.v@gmail.com>
2025-05-04 20:08:14 -0700

    Updated application.properties and SQL databases before submitting project

--- mana-schema-only.sql
+++ mana-schema-only.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `mana` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `mana`;
--- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
 -- Host: localhost    Database: mana
 -- ------------------------------------------------------
--- Server version	8.0.42
+-- Server version	9.2.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -39,7 +39,7 @@ CREATE TABLE `advert` (
   KEY `Owner_idx` (`Owner`),
   CONSTRAINT `Category` FOREIGN KEY (`Category`) REFERENCES `advertcategory` (`ID`),
   CONSTRAINT `Owner` FOREIGN KEY (`Owner`) REFERENCES `user` (`ID`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -89,4 +89,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-04 19:14:23
+-- Dump completed on 2025-05-04 20:06:14
