81794fe61a6c2a472bc7ebba6d1a9b6c515af30e
Idaliz Aviles <idalizk.v@gmail.com>
2025-05-04 18:35:06 -0700

    Fixed style and added Navigation page

--- workspace-spring-tool-suite-4-4.28.0.RELEASE/mana/src/main/java/com/inter/configuration/SecurityConfig.java
+++ workspace-spring-tool-suite-4-4.28.0.RELEASE/mana/src/main/java/com/inter/configuration/SecurityConfig.java
@@ -13,6 +13,7 @@ public class SecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(auth -> {
 			auth.requestMatchers("/").permitAll();
+			auth.requestMatchers("/navigation").permitAll();
 			auth.requestMatchers("/fragments/**").permitAll();
 			auth.requestMatchers("/images/**").permitAll();
 			auth.requestMatchers("/register").permitAll();
---
+++ workspace-spring-tool-suite-4-4.28.0.RELEASE/mana/src/main/java/com/inter/controller/NavigationController.java
@@ -0,0 +1,26 @@
+package com.inter.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.inter.helpers.SearchForm;
+import com.inter.repository.AdvertCategoryRepository;
+
+@Controller
+public class NavigationController {
+
+	@Autowired
+	AdvertCategoryRepository advertCategoryRepository;
+
+	@GetMapping("/navigation")
+	public String welcome(Map<String, Object> model) {
+		model.put("searchForm", new SearchForm());
+		model.put("advertCategories", advertCategoryRepository.findAll());
+
+		return "navigation";
+	}
+
+}
